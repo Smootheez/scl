49a5f8738a8f48d9c3e596cea1aa9c79dc412537
Smootheez <raehanstars@gmail.com>
2025-03-01 19:02:33 +0700

    Implement config file writer with JSON handling, adapter system, and registry integration

--- src/main/java/dev/smootheez/scl/file/ConfigFileWriter.java
+++ src/main/java/dev/smootheez/scl/file/ConfigFileWriter.java
@@ -1,8 +1,74 @@
 package dev.smootheez.scl.file;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import dev.smootheez.scl.config.ConfigOption;
+import dev.smootheez.scl.registry.ConfigRegistry;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class ConfigFileWriter {
+    private final Gson gson;
+    private final File configFile;
+    private final Map<String, ConfigOptionAdapter<?>> adapterMap;
+
+    public ConfigFileWriter() {
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
+        this.adapterMap = new TreeMap<>();
+
+        this.configFile = FabricLoader.getInstance().getConfigDir().resolve(ConfigRegistry.getModId() + ".json").toFile();
+
+        Set<String> usedKeys = new HashSet<>();
+        List<ConfigOption<?>> configOptions = ConfigRegistry.getConfigOptions();
+        for (ConfigOption<?> option : configOptions) {
+            String key = option.getKey();
+            if (usedKeys.contains(key)) throw new IllegalStateException("Duplicate key found: " + key);
+            usedKeys.add(key);
+            adapterMap.put(key, createAdapter(option));
+        }
+    }
+
+    public void loadConfig() {
+        if (!configFile.exists()) {
+            saveConfig();
+            return;
+        }
+
+        try (var reader = new FileReader(configFile)) {
+            JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
+            fromJson(jsonObject);
+        } catch (IOException e) {
+            throw new RuntimeException("Error load config ", e);
+        }
+    }
+
+    public void saveConfig() {
+        try (var writer = new FileWriter(configFile)){
+            JsonObject object = toJson();
+            gson.toJson(object, writer);
+        } catch (IOException e) {
+            throw new RuntimeException("Error saving config ", e);
+        }
+    }
+
+    private void fromJson(JsonObject jsonObject) {
+        adapterMap.forEach((key, adapter) -> {
+            if (jsonObject.has(key)) adapter.fromJson(jsonObject.get(key));
+        });
+    }
+
+    private JsonObject toJson() {
+        JsonObject jsonObject = new JsonObject();
+        adapterMap.forEach((key, adapter) -> jsonObject.add(key, adapter.toJson()));
+        return jsonObject;
+    }
+
     private <T> ConfigOptionAdapter<T> createAdapter(ConfigOption<T> option) {
         return new ConfigOptionAdapter<>(option);
     }
--- src/main/java/dev/smootheez/scl/registry/ConfigRegistry.java
+++ src/main/java/dev/smootheez/scl/registry/ConfigRegistry.java
@@ -3,18 +3,23 @@ package dev.smootheez.scl.registry;
 import dev.smootheez.scl.annotation.Config;
 import dev.smootheez.scl.api.ConfigProvider;
 import dev.smootheez.scl.config.ConfigOption;
+import dev.smootheez.scl.file.ConfigFileWriter;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ConfigRegistry {
     private static final List<ConfigOption<?>> configOptions = new ArrayList<>();
+    private static String modId;
+    private static final Map<Class<? extends ConfigProvider>, ConfigFileWriter> configWriters = new HashMap<>();
 
     public static <T extends ConfigProvider> void registerConfig(T config){
         var configClass = config.getClass();
         if (configClass.getAnnotation(Config.class) != null) {
-            String modId = configClass.getAnnotation(Config.class).value();
+            modId = configClass.getAnnotation(Config.class).value();
             try {
                 List<Field> configFields = getConfigFields(configClass);
                 for (Field field : configFields) {
@@ -26,7 +31,16 @@ public class ConfigRegistry {
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
-        }
+            ConfigFileWriter fileWriter = new ConfigFileWriter();
+            configWriters.put(config.getClass(), fileWriter);
+            fileWriter.loadConfig();
+        } else throw new IllegalArgumentException("ConfigFileWriter must be annotated with @Config");
+    }
+
+    public static <T extends ConfigProvider> void save(Class<T> configClass) {
+        ConfigFileWriter writer = configWriters.get(configClass);
+        if (writer != null) writer.saveConfig();
+        else throw new IllegalArgumentException("Config class not registered: " + configClass.getName());
     }
 
     private static List<Field> getConfigFields(Class<?> clazz) {
@@ -35,6 +49,10 @@ public class ConfigRegistry {
             if (field.getType().equals(ConfigOption.class)) configFields.add(field);
         }
         return configFields;
+    }
+
+    public static String getModId() {
+        return modId;
     }
 
     public static List<ConfigOption<?>> getConfigOptions() {
