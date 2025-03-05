02080910bd4d26f4ac8a4e7684cc2c18cf01501f
Smootheez <raehanstars@gmail.com>
2025-03-05 18:28:41 +0700

    remove example config screen and text config handling, update mod menu implementation

--- src/main/java/dev/smootheez/scl/example/ModMenuImpl.java
+++ src/main/java/dev/smootheez/scl/example/ModMenuImpl.java
@@ -2,10 +2,11 @@ package dev.smootheez.scl.example;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import dev.smootheez.scl.screen.ConfigScreen;
 
 public class ModMenuImpl implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return ExampleConfigScreen::new;
+        return ConfigScreen::new;
     }
 }
