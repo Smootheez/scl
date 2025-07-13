36e467327468f13547fba55c339dae01b841340e
Smootheez <raehanstars@gmail.com>
2025-07-13 12:00:14 +0700

    Replacing getFirst() with get(0) for label rendering consistency.

--- src/main/java/dev/smootheez/scl/gui/widget/entry/SliderWidgetEntry.java
+++ src/main/java/dev/smootheez/scl/gui/widget/entry/SliderWidgetEntry.java
@@ -54,12 +54,15 @@ public class SliderWidgetEntry<T extends Number> extends LabeledWidgetEntry<T> {
 
     @SuppressWarnings("unchecked")
     private T castToType(double value) {
-        return switch (option.getDefaultValue()) {
-            case Integer i -> (T) Integer.valueOf((int) value);
-            case Float v -> (T) Float.valueOf((float) value);
-            case Double v -> (T) Double.valueOf(value);
-            default -> throw new IllegalStateException("Unsupported type: " + option.getDefaultValue().getClass());
-        };
+        if (option.getDefaultValue() instanceof Integer) {
+            return (T) Integer.valueOf((int) value);
+        } else if (option.getDefaultValue() instanceof Float) {
+            return (T) Float.valueOf((float) value);
+        } else if (option.getDefaultValue() instanceof Double) {
+            return (T) Double.valueOf(value);
+        } else {
+            throw new IllegalStateException("Unsupported type: " + option.getDefaultValue().getClass());
+        }
     }
 }
 
